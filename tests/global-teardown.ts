d855a14b065eb521fd3379a2516f7e4ff903061d
nadim-bs <nadim.hossain@brainstation-23.com>
2026-05-21 19:50:34 +0600

    fixed the global tear down issue

--- tests/global-teardown.ts
+++ tests/global-teardown.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 
 test('clear session', async ({ page }) => {
     await page.goto('/');
+    await page.getByRole('link', { name: 'Log out' }).click();
     await page.context().clearCookies();
     await page.context().clearPermissions();
     await page.context().storageState({ path: './auth.json' });
